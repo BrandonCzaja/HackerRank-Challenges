f716f450edb9e3947ceea6fef4e6194079cadfee
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-23 06:55:23 -0600

    My solution for Diagonal Difference didn't pass all tests. I found this solution that does

--- CompareTheTriplets.js
+++ CompareTheTriplets.js
@@ -12,15 +12,6 @@ Comparison points is the total points a person earned.
 
 Given a and b, determine their respective comparison points.
 
-Example
-
-a = [1, 2, 3]
-b = [3, 2, 1]
-For elements *0*, Bob is awarded a point because a[0] .
-For the equal elements a[1] and b[1], no points are earned.
-Finally, for elements 2, a[2] > b[2] so Alice receives a point.
-The return array is [1, 1] with Alice's score first and Bob's second.
-
 Function Description
 
 Complete the function compareTriplets in the editor below.
