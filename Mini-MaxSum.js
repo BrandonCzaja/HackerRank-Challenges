98e68b65ff094f8acd26f8c7848a5dc8037cb557
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-24 14:36:04 -0600

    Solved Mini-MaxSum.js by sorting the array's elements in descending order

--- Mini-MaxSum.js
+++ Mini-MaxSum.js
@@ -2,3 +2,23 @@
 Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
 */
+
+const minMaxSum = (array) => {
+    array = array.sort();
+    // console.log(array);
+    let minimum = 0;
+    let maximum = 0;
+
+    for (let i = 0; i < array.length - 1; i++) {
+        minimum += array[i];
+    }
+
+    for (let i = array.length - 1; i > 0; i--) {
+        maximum += array[i];
+    }
+    // console.log(minimum);
+    // console.log(maximum);
+    console.log(`${minimum} ${maximum}`);
+};
+
+minMaxSum([1, 4, 8, 5, 2]);
